5778ba2c5b1c22809d90248ea5774c04d4b79a22
Ramazan Jalgasov <romajekk@gmail.com>
2025-12-20 19:27:36 +0500

    .

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('/admin/', admin.site.urls),
-    path('/api/', include('core.urls')),
+    path('admin/', admin.site.urls),
+    path('api/', include('core.urls')),
 
 ]
